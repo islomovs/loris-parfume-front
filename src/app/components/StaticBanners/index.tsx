4a84e74fc37b214e9a5c97c8e57f2385789febc1
Azizkhuja <azizxoja200425@mail.ru>
2024-10-04 00:15:45 +0500

    updated main page static banner images and theirs redirect urls, also changed b2b contract text

--- src/app/components/StaticBanners/index.tsx
+++ src/app/components/StaticBanners/index.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
 import React from "react";
 import banner1 from "../../../../public/staticBannerImg1.webp";
-import banner2 from "../../../../public/staticBannerImg2.webp";
-import banner3 from "../../../../public/staticBannerImg3.webp";
+import banner2 from "../../../../public/staticBannerImg2.png";
+import banner3 from "../../../../public/staticBannerImg3.png";
 import banner4 from "../../../../public/staticBannerImg4.webp";
 import Link from "next/link";
 
@@ -13,7 +13,7 @@ const StaticBanners = () => {
         <div className="grid md:gap-8 gap-4">
           <div className="grid md:grid-cols-1 grid-cols-2 md:gap-8 gap-4 h-full">
             <div className="relative overflow-hidden h-full w-full">
-              <Link href={"https://lorisparfume.uz/products/r-001"}>
+              <Link href={"https://lorisparfume.uz/collections/creation/products/creation-angel-wings"}>
                 <Image
                   src={banner1}
                   alt="banner_image1"
@@ -33,7 +33,7 @@ const StaticBanners = () => {
           </div>
           <div className="grid grid-cols-2 md:gap-8 gap-4 h-full">
             <div className="relative overflow-hidden h-full w-full">
-              <Link href={"https://lorisparfume.uz/products/r-001"}>
+              <Link href={"https://lorisparfume.uz/products/r-231"}>
                 <Image
                   src={banner2}
                   alt="banner_image2"
@@ -42,7 +42,7 @@ const StaticBanners = () => {
               </Link>
             </div>
             <div className="relative overflow-hidden h-full w-full">
-              <Link href={"https://lorisparfume.uz/products/r-001"}>
+              <Link href={"https://lorisparfume.uz/collections/elegant/products/elegant-elegant"}>
                 <Image
                   src={banner3}
                   alt="banner_image3"
@@ -53,7 +53,7 @@ const StaticBanners = () => {
           </div>
         </div>
         <div className="hidden md:block relative overflow-hidden h-full w-full">
-          <Link href={"https://lorisparfume.uz/products/r-001"}>
+          <Link href={"https://lorisparfume.uz/collections/niche"}>
             <Image
               src={banner4}
               alt="banner_image4"
